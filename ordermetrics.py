105a44ab77c2304ef825bf2b5752af9e6b827a16
Sampsa Kiiskinen <tuplanolla@gmail.com>
2018-07-18 15:46:57 +0300

    Working order metric for squares (and cubes).

--- ordermetrics.py
+++ ordermetrics.py
@@ -9,18 +9,11 @@ def ocf(rects):
     """
     x = 0
     n = 0
-
     for rect0, rect1 in it.combinations(rects, 2):
-        y = 0
-        m = 0
-
-        # This does not work.
-        # Try canonicalizing orientations.
-        for u0, u1 in zip(rect0.get_normals(), rect1.get_normals()):
-            y += np.dot(u0, u1)
-            m += 1
-
-        x += y / m
+        y = -1
+        for u0, u1 in it.product(rect0.get_normals(), rect1.get_normals()):
+            y = max(y, np.arccos(np.dot(u0, u1)))
+        nsyms = np.sqrt(rect0.get_nsyms() * rect1.get_nsyms())
+        x += np.cos(nsyms * y)
         n += 1
-
     return x / n
