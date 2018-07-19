8b85a09d4a59ecf4fd2a5ed39e2c348ed23f5710
Sampsa Kiiskinen <tuplanolla@gmail.com>
2018-07-19 11:21:56 +0300

    Order metrics for arbitrary cuboids.

--- ordermetrics.py
+++ ordermetrics.py
@@ -10,10 +10,15 @@ def ocf(rects):
     x = 0
     n = 0
     for rect0, rect1 in it.combinations(rects, 2):
-        y = -1
-        for u0, u1 in it.product(rect0.get_normals(), rect1.get_normals()):
-            y = max(y, np.arccos(np.dot(u0, u1)))
-        nsyms = np.sqrt(rect0.get_nsyms() * rect1.get_nsyms())
-        x += np.cos(nsyms * y)
-        n += 1
+        y = None
+        for (u0, n0), (u1, n1) in it.product(rect0.get_normals(), rect1.get_normals()):
+            if n0 == n1:
+                y0 = n0 * np.arccos(np.dot(u0, u1))
+                if y is None:
+                    y = y0
+                else:
+                    y = max(y, y0)
+        if not (y is None):
+            x += np.cos(y)
+            n += 1
     return x / n
